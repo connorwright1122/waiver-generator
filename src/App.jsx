27af2430e7383e0a6365cec0bcd351aaa766751b
connorwright1122 <connorwright1122@gmail.com>
2025-06-28 18:56:01 -0500

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import buzzLogo from '/default-img.png'
 import './App.css'
 import WaiverInput from './components/WaiverInput'
 import AdminPage from './components/AdminPage'
@@ -11,24 +12,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={buzzLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Buzz Studios Liability Waiver Generator</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Enter your name, GTID, and check the box to digitally sign the liability waiver. 
       </p>
       <WaiverInput/>
       <AdminPage/>
